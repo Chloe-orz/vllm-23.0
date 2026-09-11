6750cf2522d5bbc8eb755fc97afedaf8b0008e6f
qxxxw <qinxinwei1@huawei.com>
2026-09-11 14:59:22 +0800

    feat:跳过部分判断

--- vllm/engine/arg_utils.py
+++ vllm/engine/arg_utils.py
@@ -1836,7 +1836,7 @@ class EngineArgs:
             "nnodes > 1 is only supported with data_parallel_backend=mp"
         )
         inferred_data_parallel_rank = 0
-        if self.nnodes > 1:
+        if self.nnodes > 1 and False: # pp rank后续删除
             world_size = (
                 self.data_parallel_size
                 * self.pipeline_parallel_size
